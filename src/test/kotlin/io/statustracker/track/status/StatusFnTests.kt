f99a97121201d691f2b034a204e115598c3edb4e
Chris <chris.tgaunt@gmail>
2023-11-29 11:25:16 -0500

    couple tests

--- src/test/kotlin/io/statustracker/track/status/StatusFnTests.kt
+++ src/test/kotlin/io/statustracker/track/status/StatusFnTests.kt
@@ -1,9 +1,11 @@
 package io.statustracker.track.status
 
 import io.ktor.util.reflect.*
+import java.util.EmptyStackException
 import java.util.Stack
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import kotlin.test.assertTrue
 
 class StatusFnTests {
@@ -16,6 +18,28 @@ class StatusFnTests {
         assertEquals("three", result.pop())
         assertEquals("two", result.pop())
         assertEquals("one", result.pop())
-        assertEquals(0, result.size)
+        assertFailsWith(EmptyStackException::class) { result.pop() }
+    }
+
+    @Test
+    fun `buildStatusStack one status should return Stack of one String`() {
+        val subject = Status("Only")
+        val result = buildStatusStack(subject)
+
+        assertTrue { result.instanceOf(Stack::class) }
+        assertEquals("Only", result.pop())
+        assertFailsWith(EmptyStackException::class) { result.pop() }
+    }
+
+    @Test
+    fun `Status toList three nested statuses should return List of three Strings`() {
+        val subject = Status("one", Status("two", Status("three")))
+        val result = subject.toList()
+
+        assertTrue { result.instanceOf(List::class) }
+        assertEquals(result.size, 3)
+        assertEquals(result[0], "one")
+        assertEquals(result[1], "two")
+        assertEquals(result[2], "three")
     }
 }
